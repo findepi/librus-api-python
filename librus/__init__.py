285df074246b7b9b5a272982053735186654021e
michal-dbrnowski <michal.dobranowski@gmail.com>
2021-02-04 08:20:08 +0100

    Add `message_id` and `is_read` property

--- librus/__init__.py
+++ librus/__init__.py
@@ -180,19 +180,20 @@ class LibrusSession(object):
 
         for row in response.html.find('.stretch > tbody > tr'):
             cells = row.find('td')
+            href = cells[3].find('a')[0].attrs['href']
             message = Message(
+                message_id=href.strip(),
                 sender=cells[2].text,
                 subject=cells[3].text,
-                sent_at=datetime.datetime.strptime(cells[4].text, '%Y-%m-%d %H:%M:%S')
+                sent_at=datetime.datetime.strptime(cells[4].text, '%Y-%m-%d %H:%M:%S'),
+                is_read=('font-weight: bold' not in cells[3].attrs.get('style', ''))
             )
             if get_content:
-                href = cells[3].find('a')[0].attrs['href']
                 url = 'https://synergia.librus.pl' + href
                 content = self._html_session.get(url=url).html.find('.container-message-content')[0]
                 message.content = content.text
 
             yield message
-
 
 
 class Announcement(object):
@@ -253,10 +254,12 @@ class Lesson(object):
 
 
 class Message(object):
-    def __init__(self, sender, subject, sent_at):
+    def __init__(self, message_id, sender, subject, sent_at, is_read):
+        self.message_id = message_id
         self.sender = sender
         self.subject = subject
         self.sent_at = sent_at
+        self.is_read = is_read
         self.content = None
 
 
